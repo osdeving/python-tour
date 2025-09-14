3487a56f3f14d936f714c94ed3003b8cb6555e17
Willams Sousa <osdeving@gmail.com>
2025-09-14 00:34:25 +0000

    Sep 13, 2025, 9:34 PM

--- fluent-python-book/deck.py
+++ fluent-python-book/deck.py
@@ -3,4 +3,5 @@ import collections
 Card = collections.namedtuple('Card', ['rank', 'suit'])
 
 class Deck:
-    ranks = [str(n)] + 'JQKA'
+    ranks = [str(n) for n in range(1, 11)] + 'JQKA'
+    suits = ''
