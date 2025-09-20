661d0885e1b762ce22dc20d6290258f1219f8c43
Willams Sousa <osdeving@gmail.com>
2025-09-20 23:30:25 +0000

    Sep 20, 2025, 8:30 PM

--- data_visualization/main.py
+++ data_visualization/main.py
@@ -7,8 +7,8 @@ def main():
     ax.plot(squares, linewidth=3)
 
     ax.set_title('Square Numbers', fontsize=24)
-    ax.set_xlabel('Value', fontsize=24)
-    ax.set_ylabel('')
+    ax.set_xlabel('Value', fontsize=14)
+    ax.set_ylabel('Square of Value', fontsize=14)
 
     plt.show()
 
