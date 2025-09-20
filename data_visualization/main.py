94cb9904df5287d02bd4251254d121be527b3770
Willams Sousa <osdeving@gmail.com>
2025-09-20 23:25:46 +0000

    Sep 20, 2025, 8:25 PM

--- data_visualization/main.py
+++ data_visualization/main.py
@@ -1,18 +1,7 @@
 import matplotlib.pyplot as plt
 
 def main():
-    print("Hello from data-visualization!")
-
-    # Sample data
-    x = [1, 2, 3, 4, 5]
-    y = [2, 3, 5, 7, 11]
-
-    # Create a simple line plot
-    plt.plot(x, y)
-    plt.title("Sample Plot")
-    plt.xlabel("X-axis")
-    plt.ylabel("Y-axis")
-    plt.show()
+    
 
 
 if __name__ == "__main__":
