9229131c29235a35062e41f6a70c31e3f965c820
Willams Sousa <osdeving@gmail.com>
2025-09-20 23:30:40 +0000

    Sep 20, 2025, 8:30 PM

--- data_visualization/main.py
+++ data_visualization/main.py
@@ -10,6 +10,9 @@ def main():
     ax.set_xlabel('Value', fontsize=14)
     ax.set_ylabel('Square of Value', fontsize=14)
 
+    ax.tick_params(labelsize=14)
+    
+
     plt.show()
 
 if __name__ == "__main__":
