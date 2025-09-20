73b2703171971e7841fd44dde0dc33e4a9d93033
Willams Sousa <osdeving@gmail.com>
2025-09-20 23:33:54 +0000

    Sep 20, 2025, 8:33 PM

--- data_visualization/main.py
+++ data_visualization/main.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plt
 
 def main():
+    input_values = [1,2,3,4,5]
     squares = [1, 4, 9, 16, 25]
 
     fig, ax = plt.subplots()
@@ -11,7 +12,7 @@ def main():
     ax.set_ylabel('Square of Value', fontsize=14)
 
     ax.tick_params(labelsize=14)
-    
+
 
     plt.show()
 
