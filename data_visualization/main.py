e4e2203b2c91c77e0f090fea69fb0e29e869ca3e
Willams Sousa <osdeving@gmail.com>
2025-09-20 23:29:42 +0000

    Sep 20, 2025, 8:29 PM

--- data_visualization/main.py
+++ data_visualization/main.py
@@ -6,7 +6,8 @@ def main():
     fig, ax = plt.subplots()
     ax.plot(squares, linewidth=3)
 
-    ax.set_title('Square numbers')
+    ax.set_title('Square Numbers', fontsize=24)
+    ax.set
 
     plt.show()
 
