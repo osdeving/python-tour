148f7be292edfeb687a9b4e9230e17f312783e8c
Willams Sousa <osdeving@gmail.com>
2025-09-21 11:39:33 +0000

    Sep 21, 2025, 8:39 AM

--- alura/flask-demo/routes/main.py
+++ alura/flask-demo/routes/main.py
@@ -15,3 +15,8 @@ def get_products():
 def get_product_by_id(product_id):
     return jsonify({'message': f"Retorna o produto de id = {product_id}"})
 
+
+@bp.route('/products/<int:product_id>', methods=['GET'])
+def get_product_by_id(product_id):
+    return jsonify({'message': f"Retorna o produto de id = {product_id}"})
+
