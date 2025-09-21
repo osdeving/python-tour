e79fcdcb2af61a40bdf9c3455db8b7faa97bbde1
Willams Sousa <osdeving@gmail.com>
2025-09-21 11:36:32 +0000

    Sep 21, 2025, 8:36 AM

--- alura/flask-demo/routes/main.py
+++ alura/flask-demo/routes/main.py
@@ -2,7 +2,11 @@ from flask import Blueprint, jsonify
 
 bp = Blueprint('bp', __name__)
 
-@bp.route('/login', methods['POST'])
+@bp.route('/login', methods=['POST'])
 def login():
     return jsonify({"message": "Realizar login do usuário"})
 
+
+@bp.route('/products', methods=['GET'])
+def products():
+    
